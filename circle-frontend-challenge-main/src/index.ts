d8422f0513a10c57844cb50bb695490a66446bd6
Jiho <yoho4613@gmail.com>
2023-10-23 18:17:07 +1300

    script

--- circle-frontend-challenge-main/src/index.ts
+++ circle-frontend-challenge-main/src/index.ts
@@ -5,7 +5,7 @@ import ErrorHandler from './utils/ErrorHandler'
 import cors from 'cors'
 
 const app = express()
-const port = 8000
+const port = process.env.PORT || 8000
 
 app.use(express.json())
 app.use(cors())
